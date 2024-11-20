f1e8a09e9b7fb9843c82abcc81519fcce5092d14
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2024-11-20 15:22:07 +0530

    first commit and login page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Login from './pages/Login/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <BrowserRouter future={{v7_startTransition:true,v7_relativeSplatPath:true}}>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
